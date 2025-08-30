27f543d07b5fd1f037099b3a268f608001e4638c
FurinaGodly23 <tantanflores123@gmail.com>
2025-08-31 01:22:41 +0800

    Half Progress Navigation Bar

--- public/home.php
+++ public/home.php
@@ -10,8 +10,45 @@
 <body class="min-h-screen bg-gradient-to-br from-slate-900 via-indigo-900 to-indigo-800 text-white">
 
 <!-- Navigation Bar -->
- <div class="flex items-center justify-between">
+<div class="bg-slate-900 flex items-center justify-evenly">
 
- </div>
-    
+<!-- Header & Logo -->
+<div class="flex items-center text-white font-bold">
+<img src="#" class="mr-2">
+<p class="text-2xl">Github</p>
+</div>
+  
+<!-- Navigation Links -->
+<div>
+   <ul class="flex gap-6 text-gray-300">
+    <li>
+      <a href="home.php" class="flex items-center gap-2 hover:text-gray-400">
+      <img src="home.svg" alt="Home" class="w-5 h-5">Home
+      </a>
+    </li>
+    <li>
+      <a href="about.php" class="flex items-center gap-2 hover:text-gray-400">
+      <img src="about.svg" alt="About" class="w-5 h-5">About
+      </a>
+    </li>
+    <li>
+      <a href="trending.php" class="flex items-center gap-2 hover:text-gray-400">
+      <img src="trending.svg" alt="Trending" class="w-5 h-5">Trending
+      </a>
+    </li>
+    <li>
+      <a href="contact.php" class="flex items-center gap-2 hover:text-gray-400">
+      <img src="profile.svg" alt="Profile" class="w-5 h-5">Profile
+      </a>
+    </li>
+   </ul>
+</div>
+
+<!-- Profile Info & Sign Out Button(Unfinished) -->
+<div>
+<p></p>
+</div>
+
 </body>
+
+</html>
