fc315a5ba3b2532123c1a2a535c7da82ad722007
FurinaGodly23 <tantanflores123@gmail.com>
2025-08-31 00:19:18 +0800

    I created home php

---
+++ public/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Account Registration</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="min-h-screen bg-gradient-to-br from-slate-900 via-indigo-900 to-indigo-800 text-white">
+
+<!-- Navigation Bar -->
+ <div class="flex items-center justify-between">
+
+ </div>
+    
+</body>
